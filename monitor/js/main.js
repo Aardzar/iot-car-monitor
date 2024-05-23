d406c23ee65a6369abd6b5b818f9e83f1fe593e4
Aardzar <37209782+Aardzar@users.noreply.github.com>
2024-05-22 19:46:25 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -1,21 +1,19 @@
-
 let respuesta = document.getElementById("respuesta");
+let intervalId = null;
 
-function callApiRequest(){
-
-// Make a request for a user with a given ID
-    axios.get('http://3.144.160.25/iot-car-control/back-end/apis/getRegistro.php')
+function callApiRequest(ip) {
+    axios.get(`http://${ip}/iot-car-control/back-end/apis/getRegistro.php`)
     .then(function (response) {
         // handle success
         console.log(response);
 
         let respuestaServidor =
-        response.data=='w'?"Adelante": response.data=='a'?"Izquierda":
-        response.data=='s'?"Atras":response.data=='d'?"Derecha": "Detener";
+        response.data == 'w' ? "Adelante" :
+        response.data == 'a' ? "Izquierda" :
+        response.data == 's' ? "Atras" :
+        response.data == 'd' ? "Derecha" : "Detener";
         
-        respuesta.innerHTML="Respuesta: <strong>" + respuestaServidor +"</strong>";
-
-
+        respuesta.innerHTML = "Respuesta: <strong>" + respuestaServidor + "</strong>";
     })
     .catch(function (error) {
         // handle error
@@ -26,4 +24,17 @@ function callApiRequest(){
     });
 }
 
-setInterval(callApiRequest, 2000);
+function startRequests() {
+    // Clear any existing intervals
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+    }
+    
+    // Get the IP address from the input field
+    let ipAddress = document.getElementById("ip-address").value;
+    
+    // Set the interval to call the API request every 2 seconds with the given IP
+    intervalId = setInterval(function() {
+        callApiRequest(ipAddress);
+    }, 2000);
+}
